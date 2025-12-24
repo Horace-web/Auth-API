9eb4e24881f5f6bc7979aeac4fcb08698e050710
Odounlami Horace <odounlamihorace2006@gmail.com>
2025-12-25 00:34:51 +0100

    Architecture minimal du projet et installation des dépendances

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = async (req, res) => {
+  res.json({ message: 'register ok' });
+};
+
+export const login = async (req, res) => {
+  res.json({ message: 'login ok' });
+};
+
+export const logout = async (req, res) => {
+  res.json({ message: 'logout ok' });
+};
